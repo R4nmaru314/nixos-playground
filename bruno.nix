89d768703292fa701f9946e4763ba58fbe87a4f6
r4nmaru <n.lehmann@r4nmaru.de>
2024-11-23 10:22:41 +0100

    fix bruno.nix

--- bruno.nix
+++ bruno.nix
@@ -6,7 +6,8 @@
 
 let
   pname = "bruno-app";
-  version = "v1.35.0";
+  pversion = "1.35.0";
+  version = "v${pversion}";
   src = fetchurl {
     url = "https://github.com/usebruno/bruno/releases/download/${version}/bruno_1.35.0_x86_64_linux.AppImage";
     hash = "sha256-OYtB3aTcPy6LvlE6j1b/CbVUF5SG7bE2uB0nmX4cfOc=";
@@ -20,6 +21,7 @@ appimageTools.wrapType2 {
       mv $out/bin/{${pname},bruno}
       install -Dm444 ${appimageContents}/bruno.desktop -t $out/share/applications
       install -Dm444 ${appimageContents}/bruno.png -t $out/share/pixmaps
+      substituteInPlace $out/share/applications/bruno.desktop --replace-fail "AppRun" "bruno"
     '';
 
   meta = {
